a7b0cf631cc593fa50c4ff131a2462a78df5c91a
Pompy171 <77317518+Pompy171@users.noreply.github.com>
2022-02-06 12:56:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function next() {
+    window.location = "2index.html"
+}
+
+function previous1() {
+    window.location = "index.html"
+}
+
+
+function next2() {
+    window.location = "3index.html"
+}
+
+function previous2() {
+    window.location = "index.html"
+}
+
+
+function next3() {
+    window.location = "4index.html"
+}
+
+function previous3() {
+    window.location = "2index.html"
+}
+
+
+function next4() {
+    window.location = "5index.html"
+}
+
+function previous4() {
+    window.location = "3index.html"
+}
+
+function previous5() {
+    window.location = "4index.html"
+}
